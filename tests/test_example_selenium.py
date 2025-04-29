da39612e90e33f0cb5208ffd70f2bd1b466f553f
Azalea <112400084+azaliya102@users.noreply.github.com>
2025-04-29 23:10:23 +0300

    Update test_example_selenium.py

--- tests/test_example_selenium.py
+++ tests/test_example_selenium.py
@@ -9,5 +9,5 @@ def test_more_info_link(driver):
     driver.get("https://example.com")
     more_info_button = driver.find_element(By.LINK_TEXT, "More information...")
     more_info_button.click()
-    expected_url = "https://www.iana.org/help/example-domains"
+    expected_url = "https://www.iana.org/domains/example"
     assert driver.current_url == expected_url
